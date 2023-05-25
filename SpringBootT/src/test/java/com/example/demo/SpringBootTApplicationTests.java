2fb55e920071f87419f5ac720711d33510678901
xiaodengzi <xiaodengzi@pinduoduo.com>
2023-05-25 10:51:45 +0700

    test case

--- SpringBootT/src/test/java/com/example/demo/SpringBootTApplicationTests.java
+++ SpringBootT/src/test/java/com/example/demo/SpringBootTApplicationTests.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.rabbitMQ.Sender;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.security.RunAs;
 
 @SpringBootTest
 class SpringBootTApplicationTests {
-    @Test
-    void contextLoads() {
+    @Autowired
+    Sender sender;
 
+    public void before(){
+        System.out.println("before send");
+    }
+
+    public void after(){
+        System.out.println("after send");
+    }
+
+    @Test
+    public void testSend(){
+        System.out.println("testSend");
+        sender.send();
     }
 }
